c8b550f8f54de9ea86699649a05af785bb20c069
jayro <carcassonne1104@gmail.com>
2026-02-16 14:16:10 +0100

    Ajout archi multifenetre

---
+++ Location/Init.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Init
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Btn_search = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Btn_search
+        '
+        Me.Btn_search.Location = New System.Drawing.Point(375, 308)
+        Me.Btn_search.Name = "Btn_search"
+        Me.Btn_search.Size = New System.Drawing.Size(125, 37)
+        Me.Btn_search.TabIndex = 0
+        Me.Btn_search.Text = "Lancer la recherche"
+        Me.Btn_search.UseVisualStyleBackColor = True
+        '
+        'Init
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(948, 574)
+        Me.Controls.Add(Me.Btn_search)
+        Me.Name = "Init"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Btn_search As Button
+End Class
---
+++ Location/Init.vb
@@ -0,0 +1,12 @@
+﻿Public Class Init
+
+    Public Event NextWindow()
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Btn_search.Click
+        RaiseEvent NextWindow()
+    End Sub
+End Class
