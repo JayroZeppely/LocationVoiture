992e638082b0372de400910374a5ad51698acea6
jayro <carcassonne1104@gmail.com>
2026-02-17 08:20:29 +0100

    Ajout de widgets

--- Location/Init.vb
+++ Location/Init.vb
@@ -9,4 +9,12 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Btn_search.Click
         RaiseEvent NextWindow()
     End Sub
+
+    Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker1.ValueChanged
+
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs)
+
+    End Sub
 End Class
